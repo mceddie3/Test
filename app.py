8307d07fec339486048919f6218edf32d8d8d70d
MaxMcCarthy <mamccrt2@illinois.edu>
2022-09-02 10:36:48 -0600

    bug fixes

--- app.py
+++ app.py
@@ -11,15 +11,35 @@ game_data = []
 is_x_turn = True
 
 
+# determine if a user has just won the game
+def has_user_won():
+    global game_data
+
+    # add logic here
+    return False
+
+
+# update the game data with x and y
+def update_game_state(x_coordinate, y_coordinate):
+    global game_data
+    # update game data here
+
+
 @app.route('/result', methods=['POST', 'GET'])
 def result():
+    global is_x_turn
     if request.method == 'GET':
         x = request.args.get('x')
         y = request.args.get('y')
-        print(x, y)
+        update_game_state(x, y)
 
-    # flip turn with boolean logic
-    global is_x_turn
+        if has_user_won():
+            if is_x_turn:
+                return "X WON"
+            else:
+                return "O WON"
+
+    # flip who's turn it is with boolean logic
     is_x_turn = not is_x_turn
     return render_template("index.html", game_data=game_data, is_x_turn=is_x_turn)
 
@@ -31,5 +51,10 @@ def hello_world():  # put application's code here
 
 if __name__ == '__main__':
     # init game data
-    game_data = 3*[3*[""]]
+    game_data = [["" for i in range(3)] for j in range (3)]
+    is_x_turn = True
+
+    # experiment with setting game_data
+    game_data[1][2] = "THIS IS SET"
+
     app.run()
