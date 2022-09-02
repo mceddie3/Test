2b7c536580fdae7a065cddc99cbd17860f6975cd
MaxMcCarthy <mamccrt2@illinois.edu>
2022-09-02 02:39:57 -0600

    create grid

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
 
 
-@app.route('/hello')
-def hello(i):
-    print(i)
-    return 1
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'GET':
+        x = request.args.get('x')
+        y = request.args.get('y')
+        print(x,y)
+    return render_template("index.html")
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template("index.html")
-
-
-@app.route('/secret')
-def my_secret_route():
-    return 'don\'t tell'
-
-
-@app.route('/questions/<int:question_id>')    #int has been used as a filter that only integer will be passed in the url otherwise it will give a 404 error
-def find_question(question_id):
-    return ('you asked for question{0}'.format(question_id))
 
 
 if __name__ == '__main__':
